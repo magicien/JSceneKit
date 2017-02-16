34e951935cfafa424f7e83655e057b679e67d1a7
magicien <magicien.du.ballon@gmail.com>
2017-02-16 15:19:07 -0800

    add few comments

--- src/js/SceneKit/SCNView.js
+++ src/js/SceneKit/SCNView.js
@@ -12,6 +12,9 @@ export default class SCNView {
    * @constructor
    */
   constructor() {
+    /**
+     * @type {boolean}
+     */
     this.showsStatistics = false
 
     this.pointOfView = null
