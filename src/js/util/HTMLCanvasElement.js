16d007343caa366a13e1d3b9a171523c8af7dbff
magicien <magicien.du.ballon@gmail.com>
2017-04-02 13:33:41 -0700

    add definition check for HTMLCanvasElement
    and upgrade modules

--- src/js/util/HTMLCanvasElement.js
+++ src/js/util/HTMLCanvasElement.js
@@ -1,8 +1,10 @@
 'use strict'
 
 /*global HTMLCanvasElement*/
-let _HTMLCanvasElement = HTMLCanvasElement
-if(!HTMLCanvasElement){
+let _HTMLCanvasElement = null
+if(typeof HTMLCanvasElement !== 'undefined'){
+  _HTMLCanvasElement = HTMLCanvasElement
+}else{
   class HTMLCanvasElement {
     getContext(name, opt) {
       throw new Error('getContext is not implemented')
